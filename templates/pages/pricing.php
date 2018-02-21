6e969ec6b363299f74dfec4e06d754a191847a50
karolis <k.ramanauskas@hotmail.com>
2018-02-21 19:32:05 +0100

    started pricing page

--- templates/pages/pricing.php
+++ templates/pages/pricing.php
@@ -1,3 +1,35 @@
-<div class="section" id ="section7">
-	<h1>PRICING</h1>
+<div class="section" id ="pricing_section">
+	<div class="pricing_body">
+		<div class="header-block">
+			<div class="wrapper">
+				<div class="title-left">				
+				</div>
+					<div class="title-right">
+						<div class="logo-image">						
+						</div>
+					</div>
+			</div>
+		</div>
+		<section class="pricing-section-block">
+			<div class="pricing-menu">
+				
+			</div>
+				<div class="pricing-list">
+					<div id="webDev">
+						
+					</div>
+						<div id="logoDesign">
+							
+						</div>
+							<div id="c-design">
+								
+							</div>
+				</div>
+		</section>
+		<footer class="pricing-footer">
+			<div class="footer-logo">
+				
+			</div>
+		</footer>
+	</div>
 </div>
